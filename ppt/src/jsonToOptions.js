e87701854bfba9b2aa21a8820d225ac58d1f2a19
lzy49@qq.com <lzy49@qq.com>
2022-08-15 10:27:49 +0800

    fix: 修改拼接方式

--- ppt/src/jsonToOptions.js
+++ ppt/src/jsonToOptions.js
@@ -11,15 +11,22 @@ function itemToOption(json) {
       if (item.type === "text") {
         let template = document.createElement('template');
         template.innerHTML =  item.content;
-        const dom = template.content.lastChild.lastChild;
-        const fontSize = parseInt(dom.style.fontSize)
-        if(fontSize  > maxfontsize ) {
-          maxfontsize = fontSize;
-          option.context += option.title;
-          option.title = dom.textContent;
-          
-        } else {
-          option.context += dom.textContent
+        const nodes = template.content.lastChild.childNodes;
+        for( const dom of Array.from(nodes)) {
+          const fontSize = parseInt(dom.style.fontSize)
+          // 同样字体大小的是标题
+          if(fontSize === maxfontsize) {
+            option.title += dom.textContent;
+          }
+          // 如果有更大字体则替换旧的标题
+          if(fontSize  > maxfontsize ) {
+            maxfontsize = fontSize;
+            option.context += option.title;
+            option.title = dom.textContent;
+          } else {
+            // 其他都是内容
+            option.context += dom.textContent
+          }
         }
       }
       if (item.type === "image") {
