a4016787ac6b474fe90f324ba152efc8c9491b76
lzy49@qq.com <lzy49@qq.com>
2022-08-21 20:53:01 +0800

    feat: svg 扩展

--- svg-canvas-test/canvas/main.js
+++ svg-canvas-test/canvas/main.js
@@ -1,79 +1,133 @@
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
-let scale = 1;
-const rects = [
-  {
-    x: 75,
-    y: 75,
-    width: 100,
-    height: 100,
-    fill: "rgba(155,155,250,.9)",
-  },
-  {
-    x: 125,
-    y: 125,
-    width: 100,
-    height: 100,
-    fill: "rgba(250,155,155,.9)",
-  },
-];
-render();
-// 点击
-canvas.addEventListener("click", (e) => {
-  const i = catchItem(e);
-  if (i !== -1) {
-    const item = rects[i];
-    rects.splice(i, 1);
-    rects.push(item);
-    render();
+function play(rects, { clear, drawRect, context, contextInfo }) {
+  render();
+  // 点击
+  watchClick();
+  watchMove();
+  function watchMove() {
+    let path = [];
+    let scale = 1;
+    context.addEventListener("touchmove", (e) => {
+      catchPath(e);
+      if (path.length === 2) {
+        isZoom();
+      }
+    });
+    context.addEventListener("touchend", (e) => {
+      path = [];
+      rects = setScale(scale);
+      scale = 1;
+    });
+    function isZoom() {
+      const initDistance = getDistance(path[0][0], path[1][0]);
+      const distance = getDistance(
+        path[0][path[0].length - 1],
+        path[1][path[1].length - 1]
+      );
+      scale = distance / initDistance;
+      render(scale);
+    }
+    function catchPath(e) {
+      for (let i = 0; i < e.touches.length; i++) {
+        path[i] || (path[i] = []);
+        const { clientX, clientY } = e.touches[i];
+        path[i].push({
+          x: clientX,
+          y: clientY,
+        });
+      }
+    }
   }
-});
-let path = [];
-const threshold = 10;
-canvas.addEventListener("touchmove", (e) => {
-  for (let i = 0; i < e.touches.length; i++) {
-    path[i] || (path[i] = []);
-    const { clientX, clientY } = e.touches[i];
-    path[i].push({
-      x: clientX,
-      y: clientY,
+  function watchClick() {
+    context.addEventListener("click", (e) => {
+      const i = catchItem(e);
+      if (i !== -1) {
+        const item = rects[i];
+        rects.splice(i, 1);
+        rects.push(item);
+        render();
+      }
     });
   }
-  if (path.length === 2) {
-    const initDistance = getDistance(
-      {
-        x: path[0][0].clientX,
-        y: path[0][0].clientY,
-      },
-      {
-        x: path[1][0].clientX,
-        y: path[1][0].clientY,
+  // 计算点在那个图形中
+  function catchItem(e) {
+    let { clientX, clientY } = e;
+    clientY = clientY - contextInfo.y;
+    clientX = clientX - contextInfo.x;
+    for (let i = rects.length - 1; i >= 0; i--) {
+      const item = rects[i];
+      const { x, y, width, height } = item;
+      if (clientX - x > 0 && clientX - x < width) {
+        if (clientY - y > 0 && clientY - y < height) {
+          return i;
+        }
       }
-    );
-    const distance = getDistance(
-      {
-        x: path[0][path.length].clientX,
-        y: path[0][path.length].clientY,
-      },
-      {
-        x: path[1][path.length].clientX,
-        y: path[1][path.length].clientY,
-      }
-    );
-    scale = isNaN(initDistance / distance) ? 0 : initDistance / distance 
-    console.log(initDistance, distance)
-    log(scale)
-    render();
-    return;
+    }
+    return -1;
   }
-  if (path.length === 1) {
-    // 移动
-    return;
+  function render(scale = 1) {
+    clear();
+    drawItems(scale);
+    function drawItems(scale) {
+      setScale(scale).forEach(drawRect);
+    }
   }
-});
-canvas.addEventListener("touchend", (e) => {
-  path = [];
-});
-function log(str){
-  document.getElementById('logs').innerText = str;
+  function setScale(scale) {
+    return rects.map((item) => {
+      const { x, y, width, height } = item;
+      return {
+        ...item,
+        x: x - (width * (scale - 1)) / 2,
+        y: y - (height * (scale - 1)) / 2,
+        width: width * scale,
+        height: height * scale,
+      };
+    });
+  }
+  function getDistance(p1, p2) {
+    let dep = Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
+    return dep;
+  }
+}
+
+function createContext() {
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext("2d");
+  function clear() {
+    let w = canvas.width;
+    let h = canvas.height;
+    ctx.clearRect(0, 0, w, h);
+  }
+  function drawRect(aRect) {
+    ctx.fillStyle = aRect.fill;
+    ctx.fillRect(aRect.x, aRect.y, aRect.width, aRect.height);
+  }
+  return {
+    clear,
+    drawRect,
+    contextInfo: canvas.getBoundingClientRect(),
+    context: canvas,
+  };
+}
+function createSvgContext() {
+  const svg = document.getElementById("svg");
+  console.log(svg);
+  function clear() {
+    svg.innerHTML = "";
+  }
+  function drawRect(aRect) {
+    const rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+    rect.style.fill = aRect.fill;
+    rect.style.pointerEvents = "none";
+    rect.setAttribute("x", aRect.x);
+    rect.setAttribute("y", aRect.y);
+    rect.setAttribute("width", aRect.width);
+    rect.setAttribute("height", aRect.height);
+    svg.appendChild(rect);
+  }
+  return {
+    clear,
+    drawRect,
+    contextInfo: svg.getBoundingClientRect(),
+    context: svg,
+  };
 }
