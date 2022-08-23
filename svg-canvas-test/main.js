0f0ed6d3f56f5a1e932e0cfce98fb48e2f1ae3bf
lzy49@qq.com <lzy49@qq.com>
2022-08-23 16:13:36 +0800

    feat: 增加 safari 缩放禁止

--- svg-canvas-test/main.js
+++ svg-canvas-test/main.js
@@ -131,3 +131,21 @@ function createSvgContext() {
     addEvent:svg.addEventListener,
   };
 }
+window.onload=function () {
+  document.addEventListener('touchstart',function (event) {
+    if(event.touches.length>1){
+      event.preventDefault();
+    }
+  });
+  var lastTouchEnd=0;
+  document.addEventListener('touchend',function (event) {
+    var now=(new Date()).getTime();
+    if(now-lastTouchEnd<=300){
+      event.preventDefault();
+    }
+    lastTouchEnd=now;
+  },false);
+  document.addEventListener('gesturestart', function (event) {
+    event.preventDefault();
+  });
+}
